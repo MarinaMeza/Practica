f10c90135f36b5351811f86036422f2346f1c86f
MarinaMeza <kaiodelnorte@yahoo.com.ar>
2015-09-22 00:04:44 -0300

    practica ej 39

---
+++ Fabrica.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Practica
+{
+    class Fabrica
+    {
+        private Operario[] _operarios;
+        private string _razonSocial;
+
+        public Fabrica()
+        {
+            this._operarios=new Operario[5];
+        }
+
+        public Fabrica(string razonSocial): this()
+        {
+            this._razonSocial = razonSocial;
+        }
+
+        public string Mostrar()
+        {
+            return ("\nRazon social: "+this._razonSocial+"\n\n"+this.MostrarOperarios());
+        }
+
+        private float RetornarCostos()
+        {
+            float acumulador = 0;
+
+            for (int i = 0; i < this._operarios.Length; i++)
+            {
+                acumulador = acumulador + this._operarios[i].getSalario();
+            }
+
+            return acumulador;
+        }
+
+        private int ObtenerIndice()
+        {
+            for (int i = 0; i < this._operarios.Length; i++)
+            {
+                if (this._operarios.GetValue(i) == null)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        private int ObtenerIndice(Operario op)
+        {
+            for (int i = 0; i < this._operarios.Length; i++)
+            {
+                if (this._operarios.GetValue(i) == op)
+                {
+                    return i;
+                }
+            } 
+            return -1;
+        }
+
+        private string MostrarOperarios()
+        {
+            string todos = null;
+
+            for (int i = 0; i < this._operarios.Length; i++)
+            {
+                todos = todos + this._operarios[i].Mostrar();
+            }
+            return todos;
+        }
+
+        public static void MostrarCosto(Fabrica fbr)
+        {
+            Console.WriteLine(fbr.RetornarCostos());
+        }
+
+        public static bool operator ==(Fabrica fbr, Operario op)
+        {
+            for (int i = 0; i < fbr._operarios.Length ; i++)
+            {
+                if (fbr._operarios.GetValue(i) == op)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool operator !=(Fabrica fbr, Operario op)
+        {
+            return !(fbr == op);
+        }
+
+        public static Fabrica operator +(Fabrica fbr, Operario op)
+        {
+            if (fbr.ObtenerIndice() == -1)
+            {
+                Console.WriteLine("No hay mas cupo!");
+            }
+            else
+            {
+                if (fbr == op)
+                {
+                    Console.WriteLine("No se encontro al operario!");
+                }
+                else
+                {
+                    fbr._operarios[fbr.ObtenerIndice()] = op;
+                }
+            }
+            return fbr;
+        }
+
+        public static Fabrica operator -(Fabrica fbr, Operario op)
+        {
+            if (fbr.ObtenerIndice() != -1)
+            {
+                if (fbr == op)
+                {
+                    fbr._operarios[fbr.ObtenerIndice()] = null;
+                }
+            }
+            return fbr;
+        }
+    }
+}
---
+++ Operario.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Practica
+{
+    class Operario
+    {
+        private string _apellido;
+        private int _legajo;
+        private string _nombre;
+        private float _salario;
+
+        public Operario()
+        {
+            this._salario = 1500;
+        }
+
+        public Operario(string nombre, string apellido): this()
+        {
+            this._nombre = nombre;
+            this._apellido = apellido;
+        }
+
+        public Operario(string nombre, string apellido, int legajo): this(nombre, apellido)
+        {
+            this._legajo = legajo;
+        }
+
+        public float getSalario()
+        {
+            return this._salario;
+        }
+
+        public void setAumentarSalario(float aumento)
+        {
+            this._salario = this._salario + (this._salario * (aumento / 100));
+        }
+
+        public string ObtenerNombreYApellido()
+        {
+            return this._nombre + ", " + this._apellido;
+        }
+
+        public string Mostrar()
+        {
+            return this.ObtenerNombreYApellido() + "\nLegajo: " + this._legajo + "\nSalario: " + this._salario+"\n";
+        }
+
+        public static string MostrarR(Operario op)
+        {
+            return op.Mostrar();
+        }
+
+        public static bool operator ==(Operario op1, Operario op2)
+        {
+            if (op1.ObtenerNombreYApellido() == op2.ObtenerNombreYApellido())
+            {
+                if (op1._legajo == op2._legajo)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool operator !=(Operario op1, Operario op2)
+        {
+            return !(op1 == op2);
+        }
+    }
+}
